6197f4267fb33735f000fbd2204f5d790c4cbcde
Noah Pittman <noahpittman00@gmail.com>
2024-02-12 13:57:30 -0500

    Refactor mode toggle logic

--- src/components/ui/mode-toggle.tsx
+++ src/components/ui/mode-toggle.tsx
@@ -3,15 +3,20 @@ import { SquareIcon } from "lucide-react";
 import { motion } from "framer-motion";
 
 export function ModeToggle() {
-	const { setTheme, theme } = useTheme();
+	const { setTheme, theme, systemTheme } = useTheme();
+
+	const toggleTheme = () => {
+		if (theme === "system") {
+			systemTheme === "light" ? setTheme("dark") : setTheme("light");
+		} else theme === "light" ? setTheme("dark") : setTheme("light");
+	};
 
 	return (
 		<motion.button
 			whileTap={{ translateY: 2 }}
 			className="focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-ring/0 disabled:pointer-events-none disabled:opacity-50 inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors h-9 w-9"
 			onClick={() => {
-				if (theme == "light") setTheme("dark");
-				else setTheme("light");
+				toggleTheme();
 			}}
 		>
 			<SquareIcon className="w-5 h-5 fill-foreground" />
