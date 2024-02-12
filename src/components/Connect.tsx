ad76a074c4221b28c4c6ab0f30db9dfde7c9b287
Noah Pittman <noahpittman00@gmail.com>
2024-02-11 21:10:22 -0500

    Update Connect component styling

--- src/components/Connect.tsx
+++ src/components/Connect.tsx
@@ -5,7 +5,7 @@ const Connect = () => {
 	return (
 		<section id="connect">
 			<h2 className="text-lg font-bold">Connect</h2>
-			<div className="flex flex-col space-y-1 ">
+			<div className="flex flex-col space-y-1 sm:text-base text-sm ">
 				<div className="flex gap-4">
 					<p className="max-w-[6ch] w-full">Twitter</p>
 					<Link
@@ -13,7 +13,7 @@ const Connect = () => {
 						href={"https://twitter.com/_pittman"}
 						target="_blank"
 					>
-						@_pittman
+						<p className="truncate">@_pittman</p>
 						<ArrowUpRight className="h-4 w-4 ml-1" />
 					</Link>
 				</div>
@@ -24,18 +24,18 @@ const Connect = () => {
 						href={"https://github.com/noahpittman"}
 						target="_blank"
 					>
-						<p>@noahpittman</p>
+						<p className="truncate">@noahpittman</p>
 						<ArrowUpRight className="h-4 w-4 ml-1" />
 					</Link>
 				</div>
 				<div className="flex gap-4">
 					<p className="max-w-[6ch] w-full">Email</p>
 					<Link
-						className="flex items-center decoration-1 text-muted-foreground decoration-muted-foreground hover:text-foreground hover:decoration-foreground transition-colors border-b border-muted-foreground/0 hover:border-foreground "
+						className="flex items-center decoration-1 text-muted-foreground decoration-muted-foreground hover:text-foreground hover:decoration-foreground transition-colors border-b border-muted-foreground/0 hover:border-foreground"
 						href={"mailto:noahpittman00@gmail.com"}
 						target="_blank"
 					>
-						<p>noahpittman[zero][zero]@gmail.com</p>
+						<p className="truncate">noahpittman[zero][zero]@gmail.com</p>
 						<ArrowUpRight className="h-4 w-4 ml-1" />
 					</Link>
 				</div>
