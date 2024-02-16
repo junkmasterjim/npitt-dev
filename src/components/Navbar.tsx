9b237c93fe5caf1e52840f8d99fdbcca199df50e
Noah Pittman <noahpittman00@gmail.com>
2024-02-16 11:04:48 -0500

    Refactor Navbar component and add dynamic tabs

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,20 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import { Button } from "./ui/button";
 import { ModeToggle } from "./ui/mode-toggle";
 import { motion } from "framer-motion";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
+import { useState } from "react";
+
+const tabs: { label: string; id: string; href: string }[] = [
+	{ label: "Home", id: "home", href: "/" },
+	{ label: "[ . . . ]", id: "dotdotdot", href: "/dotdotdot" },
+];
 
 const Navbar = () => {
 	const pathname = usePathname();
+	const [activeTab, setActiveTab] = useState(pathname.split("/")[1] || "home");
 
 	return (
 		<nav className="fixed inset-x-0 bottom-4 w-full px-2">
@@ -18,28 +24,35 @@ const Navbar = () => {
 				transition={{ duration: 0.5 }}
 				className="bg-secondary shadow-sm relative mx-auto justify-evenly items-center  max-w-xs p-2 rounded-full px-6 border border-border/50"
 			>
-				<div className="flex items-center justify-evenly  w-full gap-2 mx-auto">
-					<Button
-						asChild
-						variant={"ghost"}
-						className={cn(
-							pathname === "/" && "bg-accent/75 hover:bg-accent/75"
-						)}
-					>
-						<Link prefetch href={"/"}>
-							Home
+				<div className="flex gap-2 justify-evenly items-center">
+					{tabs.map((tab) => (
+						<Link prefetch aria-label={tab.id} key={tab.id} href={tab.href}>
+							<button
+								name={tab.id}
+								key={tab.id}
+								onClick={() => setActiveTab(tab.id)}
+								className={cn(
+									"inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
+									activeTab === tab.id ? "" : "hover:bg-accent/75",
+									"hover:text-accent-foreground relative h-9 px-4 py-2"
+								)}
+								style={{
+									WebkitTapHighlightColor: "transparent",
+								}}
+							>
+								{tab.label}
+								{/* Active tab span */}
+								{activeTab === tab.id && (
+									<motion.span
+										layoutId="bubble"
+										className="absolute inset-0 z-10 bg-accent/75 mix-blend-overlay"
+										style={{ borderRadius: 6 }}
+										transition={{ type: "spring", bounce: 0.1, duration: 0.5 }}
+									/>
+								)}
+							</button>
 						</Link>
-					</Button>
-					<Button
-						asChild
-						variant={"ghost"}
-						className={cn(
-							pathname.includes("/dotdotdot") &&
-								"bg-accent/75 hover:bg-accent/75"
-						)}
-					>
-						<Link href={"/dotdotdot"}>{`[ . . . ]`}</Link>
-					</Button>
+					))}
 					<ModeToggle />
 				</div>
 			</motion.div>
