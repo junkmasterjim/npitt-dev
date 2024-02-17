39515915b5f3b42670ee63ac22e89395a0b1e9bc
Noah Pittman <noahpittman00@gmail.com>
2024-02-17 13:06:46 -0500

    animate code reveal

--- src/app/dotdotdot/_components/layout/CodePreview.tsx
+++ src/app/dotdotdot/_components/layout/CodePreview.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { ClipboardCheck, ClipboardEdit } from "lucide-react";
+import { AnimatePresence, motion } from "framer-motion";
 
 export const CodePreview = ({ code }: { code: string }) => {
 	const [showCode, setShowCode] = useState<boolean>(false);
@@ -19,36 +20,49 @@ export const CodePreview = ({ code }: { code: string }) => {
 
 	return (
 		<div>
-			<div
-				className={cn(
-					"overflow-hidden h-[256px] bg-[#111] text-[#aaa] border border-[#444]/20  rounded-md transition-all",
-					showCode && "h-auto text-white"
-				)}
-			>
-				<div className="flex flex-row-reverse h-0">
-					<Button
-						disabled={copying}
-						className="relative top-3 right-5 bg-[#444] text-[#ccc] hover:bg-[#555] hover:text-[#fff] "
-						size={"icon"}
-						onClick={handleCopy}
-					>
-						{!copying ? (
-							<ClipboardEdit className="h-5 w-5" />
-						) : (
-							<ClipboardCheck className="h-5 w-5" />
-						)}
-					</Button>
-				</div>
+			<AnimatePresence mode="popLayout">
+				<motion.div
+					layout
+					initial={{ height: 256, color: "#aaa" }}
+					animate={{
+						height: showCode ? "auto" : 256,
+						color: showCode ? "#fff" : "#aaa",
+					}}
+					className="overflow-hidden bg-[#111] border border-[#444]/20 rounded-md transition"
+				>
+					<div className="flex flex-row-reverse h-0">
+						<Button
+							disabled={copying}
+							className="relative top-3 right-5 bg-[#444] text-[#ccc] hover:bg-[#555] hover:text-[#fff] "
+							size={"icon"}
+							onClick={handleCopy}
+						>
+							{!copying ? (
+								<ClipboardEdit className="h-5 w-5" />
+							) : (
+								<ClipboardCheck className="h-5 w-5" />
+							)}
+						</Button>
+					</div>
 
-				<pre className="overflow-auto overflow-y-hidden">
-					<code className="text-xs p-8 font-mono">{code}</code>
-				</pre>
-			</div>
+					<pre className="overflow-auto overflow-y-hidden">
+						<code className="text-xs p-8 font-mono">{code}</code>
+					</pre>
+				</motion.div>
+			</AnimatePresence>
 			<div className="relative flex justify-center -top-[18px]">
 				<Button
 					className="border-input border-1 bg-white text-black hover:bg-[#eee] shadow"
 					onClick={() => {
-						setShowCode(!showCode);
+						if (showCode) {
+							window.scroll({
+								top: 0,
+								behavior: "smooth",
+							});
+							setTimeout(() => {
+								setShowCode(!showCode);
+							}, 750);
+						} else setShowCode(!showCode);
 					}}
 				>
 					{!showCode ? "Show" : "Hide"}
