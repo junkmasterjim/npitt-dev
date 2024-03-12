8c044b30e1b439a7a1790c353d32b943310a6b9e
Noah Pittman <noahpittman00@gmail.com>
2024-03-12 16:05:48 -0400

    Add new features and links to Projects component

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,25 +1,34 @@
 import Link from "next/link";
 import { Badge } from "./ui/badge";
 import { FaHashtag } from "react-icons/fa";
+import { Button } from "./ui/button";
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
+import { Download } from "lucide-react";
 
 const projects: {
 	href: string;
 	title: string;
 	description: string;
 	stack: string[];
+	repo?: string;
+	download?: string;
 }[] = [
 	{
 		href: "https://pomologs.org",
 		title: "PomoLogs - Pomodoro Timer & Work Logs",
 		description:
 			"Combines a traditional pomodoro timer with work logs. Downloadable as a PWA with Tauri.",
-		stack: ["Next.js", "Tailwind CSS"],
+		stack: ["Next.js", "Tailwind CSS", "Tauri"],
+		repo: "https://github.com/noahpittman/pomologs-tauri",
+		download:
+			"https://www.dropbox.com/scl/fi/0d003v63e6t6una1sysxj/PomoLogs.zip?rlkey=sgb6h0kdpgdiu47zdo21vui4f&dl=1",
 	},
 	{
 		href: "https://devpillar.org",
 		title: "DevPillar - Resource Library",
 		description: "Hand-picked resource library for developers & designers.",
-		stack: ["Next.js", "Tailwind CSS", "PostgreSQL", "Auth"],
+		stack: ["Next.ts", "Tailwind CSS", "PostgreSQL", "Auth"],
+		repo: "https://github.com/noahpittman/devpillar",
 	},
 	// {
 
@@ -35,6 +44,7 @@ const projects: {
 		title: "DoneByHD - Portfolio",
 		description: "Portfolio website for a freelance designer.",
 		stack: ["Next.js", "Tailwind CSS", "Prisma", "MySQL", "Auth"],
+		repo: "https://github.com/noahpittman/donebyhd",
 	},
 ];
 
@@ -57,25 +67,58 @@ const Projects = () => {
 			</h2>
 
 			<div className="grid grid-cols-1 gap-4">
-				{projects.map((project) => (
-					<Link
-						key={project.title}
-						target="_blank"
-						href={project.href}
-						className="md:hover:scale-[1.01] bg-primary/25 md:hover:bg-primary/50 dark:bg-primary-foreground/20 dark:md:hover:bg-primary/10 rounded-lg px-4 py-3 transition-all space-y-2
-							border-border/10 dark:border-border/10 border  dark:shadow-accent/10 hover:shadow
-							"
+				{projects.map((project, i) => (
+					<div
+						className={`flex ${
+							i % 2 === 0 ? "sm:flex-row" : "sm:flex-row-reverse"
+						} gap-4 flex-col`}
+						key={i}
 					>
-						<h3 className="text-lg font-semibold">{project.title}</h3>
-						<p className="text-muted-foreground text-sm">
-							{project.description}
-						</p>
-						<div className="flex flex-wrap items-center gap-4">
-							{project.stack.map((tech) => (
-								<Badge key={tech}>{tech}</Badge>
-							))}
-						</div>
-					</Link>
+						<Link
+							key={project.title}
+							target="_blank"
+							href={project.href}
+							className="md:hover:scale-[1.01] bg-primary/25 md:hover:bg-primary/50 dark:bg-primary-foreground/20 dark:md:hover:bg-primary/10 rounded-lg px-4 py-3 transition-all space-y-2
+							border-border/10 dark:border-border/10 border  dark:shadow-accent/10 hover:shadow sm:w-3/4"
+						>
+							<h3 className="text-lg font-semibold">{project.title}</h3>
+							<p className="text-muted-foreground text-sm">
+								{project.description}
+							</p>
+							<div className="flex flex-wrap items-center gap-4">
+								{project.stack.map((tech) => (
+									<Badge key={tech}>{tech}</Badge>
+								))}
+							</div>
+						</Link>
+						{(project.repo || project.download) && (
+							<div
+								className="sm:w-1/4 md:hover:scale-[1.01] bg-primary/25 md:hover:bg-primary/50 dark:bg-primary-foreground/20 dark:md:hover:bg-primary/10 rounded-lg px-4 py-3 transition-all
+							border-border/10 dark:border-border/10 border  dark:shadow-accent/10 hover:shadow flex flex-row sm:flex-col justify-center items-center gap-4 flex-wrap"
+							>
+								{project.repo && (
+									<Button asChild variant={"ghost"}>
+										<Link href={project.repo} target="_blank">
+											<GitHubLogoIcon className="size-5 mr-2" />
+											Repo
+										</Link>
+									</Button>
+								)}
+								{project.download && (
+									<Button asChild variant={"ghost"}>
+										<Link
+											href={project.download}
+											target="_blank"
+											className="self-center justify-self-center"
+										>
+											<Download className="size-5 mr-2" />
+											Download
+										</Link>
+									</Button>
+								)}
+							</div>
+						)}
+					</div>
 				))}
 			</div>
 		</section>
