28652d22a60b63b004d358321d469875ea732078
Noah Pittman <noahpittman00@gmail.com>
2024-10-15 11:09:02 -0300

    update theme

--- components/section-heading.tsx
+++ components/section-heading.tsx
@@ -1,7 +1,7 @@
 export const SectionHeading = ({ children }: { children: React.ReactNode }) => {
-	return (
-		<h3 className="text-sm uppercase text-muted-foreground tracking-wider">
-			{children}
-		</h3>
-	);
+  return (
+    <h3 className="text-sm uppercase text-muted-foreground tracking-wider">
+      {children}
+    </h3>
+  );
 };
