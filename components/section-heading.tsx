2771d5b13dfa21b762d14e84e06eea03e13435c0
junkmasterjim <noahpittman00@gmail.com>
2025-11-01 15:10:22 -0300

    init payload app

--- components/section-heading.tsx
+++
@@ -1,7 +0,0 @@
-export const SectionHeading = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <h3 className="text-sm uppercase text-muted-foreground tracking-wider">
-      {children}
-    </h3>
-  );
-};
