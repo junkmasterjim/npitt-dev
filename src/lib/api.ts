f9e247bd6a01632ad9a105eaf904c3fd5fa0a52a
junkmasterjim <noahpittman00@gmail.com>
2025-11-03 19:41:49 -0400

    test for build

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -9,7 +9,12 @@ export async function fetchCMSData(): Promise<CMSData> {
     cache: 'force-cache',
   })
 
-  if (!res.ok) throw new Error('Failed to fetch CMS data')
+  // if (!res.ok) throw new Error('Failed to fetch CMS data')
+  if (!res.ok) {
+    const test: CMSData = { projects: [], media: [], homeContent: [], posts: [] }
+    return test;
+  }
+
   return res.json()
 }
 
