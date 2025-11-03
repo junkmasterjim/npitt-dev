bec38cc2350b3ca2dd20bbf6293150e632745967
junkmasterjim <noahpittman00@gmail.com>
2025-11-02 23:44:58 -0400

    frontend: update navbar

---
+++ src/app/(frontend)/projects/page.tsx
@@ -0,0 +1,51 @@
+"use client"
+
+import PageContainer from "@/components/custom/page-container"
+import TooltipLink from "@/components/custom/tooltip-link"
+import { Badge } from "@/components/ui/badge"
+import { fetchCMSData, CMSData } from "@/lib/api"
+import { formatPayloadDate } from "@/lib/utils"
+import { Project } from "@/payload-types"
+import { useQuery } from "@tanstack/react-query"
+import { Code2, Globe } from "lucide-react"
+
+export default function Page() {
+  const { data } = useQuery({
+    queryKey: ['cms-data'],
+    queryFn: fetchCMSData,
+  })
+
+  const d: CMSData = data
+  const projects: Array<Project> = d.projects;
+
+
+  return (
+    <PageContainer>
+      <ul>
+        {projects.map((p: Project, i) => {
+
+          const s: Array<{ name: string }> = p.stack as Array<{ name: string }>
+
+          return (
+            <li key={i}>
+              <div className="flex items-center gap-2">
+                <h4 className="text-md tracking-tighter italic">{p.title}</h4>
+                <TooltipLink href={p["project link"]} label="Live Website" content={<Globe size={16} />} />
+                <TooltipLink href={p["repo link"]} label="Code Repository" content={<Code2 size={16} />} />
+              </div>
+              <p className="text-sm tracking-tighter -mt-1 text-foreground/70">{formatPayloadDate(p.date)}</p>
+              <p>{p.description}</p>
+              <div className="flex items-center gap-1">
+                {s.map((tech: any, i: number) => (
+                  <Badge className="rounded-none" key={i}>{tech.name}</Badge>
+                ))}
+              </div>
+            </li>
+          )
+        })}
+      </ul>
+    </PageContainer >
+  )
+
+}
+
--- src/components/custom/navbar.tsx
+++ src/components/custom/navbar.tsx
@@ -10,9 +10,8 @@ export default function Navbar() {
   const pathname = usePathname();
 
   return (
-
     <div className="flex items-center justify-between px-3 py-2 w-full border-b">
-      <nav className="flex items-center gap-2">
+      <nav className="flex items-center gap-4">
         <SlidingLink className={cn(pathname == "/" && "font-semibold")} href={"/"}>Home</SlidingLink>
         <SlidingLink className={cn(pathname == "/projects" && "font-semibold")} href={"/projects"}>Projects</SlidingLink>
         <SlidingLink className={cn(pathname == "/writing" && "font-semibold")} href={"/writing"}>Writing</SlidingLink>
