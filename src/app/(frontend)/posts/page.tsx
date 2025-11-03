f980e98260878933411f2237c138af99a56193b5
junkmasterjim <noahpittman00@gmail.com>
2025-11-03 19:47:30 -0400

    conditional rendering

--- src/app/(frontend)/posts/page.tsx
+++ src/app/(frontend)/posts/page.tsx
@@ -7,20 +7,19 @@ import { SlidingLink } from "@/components/custom/sliding-link";
 export default function PostsPage() {
   const { posts } = useCMS();
 
-  console.log(posts)
-
-  return (
-    <PageContainer>
-      <div>
-        {posts.map((p) => (
-          <div key={p.id}>
-            <SlidingLink href={`/posts/${p.slug}`}>
-              {p.type}: {p.slug}
-            </SlidingLink>
-          </div>
-        ))}
-      </div>
-    </PageContainer>
-  )
+  if (posts != undefined)
+    return (
+      <PageContainer>
+        <div>
+          {posts.map((p) => (
+            <div key={p.id}>
+              <SlidingLink href={`/posts/${p.slug}`}>
+                {p.type}: {p.slug}
+              </SlidingLink>
+            </div>
+          ))}
+        </div>
+      </PageContainer>
+    )
 }
 
--- src/app/(frontend)/projects/page.tsx
+++ src/app/(frontend)/projects/page.tsx
@@ -12,33 +12,34 @@ export default function ProjectsPage() {
   const { projects }: { projects: Array<Project> } = useCMS();
   const featuredProjects = projects.filter((proj) => proj["is featured"] == true);
 
-  return (
-    <PageContainer>
-      <ul>
-        {projects.map((p: Project, i) => {
+  if (featuredProjects != undefined)
+    return (
+      <PageContainer>
+        <ul>
+          {projects.map((p: Project, i) => {
 
-          const s: Array<{ name: string }> = p.stack as Array<{ name: string }>
+            const s: Array<{ name: string }> = p.stack as Array<{ name: string }>
 
-          return (
-            <li key={i}>
-              <div className="flex items-center gap-2">
-                <h4 className="text-md tracking-tighter italic">{p.title}</h4>
-                <TooltipLink href={p["project link"]} label="Live Website" content={<Globe size={16} />} />
-                <TooltipLink href={p["repo link"]} label="Code Repository" content={<Code2 size={16} />} />
-              </div>
-              <p className="text-sm tracking-tighter -mt-1 text-foreground/70">{formatPayloadDate(p.date)}</p>
-              <p>{p.description}</p>
-              <div className="flex items-center gap-1">
-                {s.map((tech: any, i: number) => (
-                  <Badge className="rounded-none" key={i}>{tech.name}</Badge>
-                ))}
-              </div>
-            </li>
-          )
-        })}
-      </ul>
-    </PageContainer >
-  )
+            return (
+              <li key={i}>
+                <div className="flex items-center gap-2">
+                  <h4 className="text-md tracking-tighter italic">{p.title}</h4>
+                  <TooltipLink href={p["project link"]} label="Live Website" content={<Globe size={16} />} />
+                  <TooltipLink href={p["repo link"]} label="Code Repository" content={<Code2 size={16} />} />
+                </div>
+                <p className="text-sm tracking-tighter -mt-1 text-foreground/70">{formatPayloadDate(p.date)}</p>
+                <p>{p.description}</p>
+                <div className="flex items-center gap-1">
+                  {s.map((tech: any, i: number) => (
+                    <Badge className="rounded-none" key={i}>{tech.name}</Badge>
+                  ))}
+                </div>
+              </li>
+            )
+          })}
+        </ul>
+      </PageContainer >
+    )
 
 }
 
